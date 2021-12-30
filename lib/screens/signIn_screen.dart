b5a8e208a3afa2c9609c88324505396045b97541
Pranav Pratap Singh <pranavpratapsingh108@gmail.com>
2021-12-30 16:06:10 +0530

    Completed SignIn Screen

--- lib/screens/signIn_screen.dart
+++ lib/screens/signIn_screen.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SignInScreen extends StatefulWidget {
@@ -8,8 +11,183 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  late String _email;
+  late String _password;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(
+      children: [
+        Image.asset(
+          "images/img.png",
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          fit: BoxFit.cover,
+        ),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Center(
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  Expanded(
+                    flex: 7,
+                    child: Container(
+                      padding: const EdgeInsets.all(0),
+                      child: Center(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: const [
+                            Text(
+                              'Audio',
+                              style:
+                                  TextStyle(fontSize: 40, color: Colors.white),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              "It's modular and designed to last",
+                              style:
+                                  TextStyle(fontSize: 20, color: Colors.white),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 3,
+                    child: Container(),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(0),
+                    child: Column(
+                      children: [
+                        ListTile(
+                          horizontalTitleGap: 8.0,
+                          tileColor: Colors.white,
+                          leading: Icon(Icons.email_outlined),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          title: TextField(
+                            onChanged: (value) {
+                              setState(() {
+                                _email = value;
+                              });
+                            },
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Email',
+                              hintStyle: TextStyle(
+                                color: Colors.grey,
+                              ),
+                            ),
+                            textAlign: TextAlign.start,
+                            keyboardType: TextInputType.emailAddress,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        ListTile(
+                          horizontalTitleGap: 8.0,
+                          tileColor: Colors.white,
+                          leading: Icon(Icons.lock),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          title: TextField(
+                            onChanged: (value) {
+                              setState(() {
+                                _password = value;
+                              });
+                            },
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Password',
+                              hintStyle: TextStyle(
+                                color: Colors.grey,
+                              ),
+                            ),
+                            textAlign: TextAlign.start,
+                            obscureText: true,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        TextButton(
+                            style: TextButton.styleFrom(
+                              backgroundColor: Colors.transparent,
+                            ),
+                            onPressed: () {},
+                            child: Text(
+                              'Forgot password',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 20),
+                            )),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        TextButton(
+                            style: TextButton.styleFrom(
+                              fixedSize: Size(double.maxFinite, 50),
+                              backgroundColor: Colors.green,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                            ),
+                            onPressed: () {},
+                            child: Text(
+                              'Sign In',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 20),
+                            )),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Don't have any account?",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 18),
+                            ),
+                            GestureDetector(
+                              onTap: () {},
+                              child: Text(
+                                'Sign Up here',
+                                style: TextStyle(
+                                  color: Colors.green,
+                                  fontSize: 20,
+                                  decoration: TextDecoration.underline,
+                                ),
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
